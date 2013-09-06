8e2cadfd994a00809da4363906379b542e472f80
Nahuel Tori <nahueltori@gmail.com>
2013-09-06 17:59:21 -0300

    Update Pais.java
    Avanza la clase Pais.

--- modelos7114-tp1/src/modelos/tp1/Pais.java
+++ modelos7114-tp1/src/modelos/tp1/Pais.java
@@ -4,11 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Pais {
+	int codigo;
 	boolean cobertura;
 	int limSinCobertura;
 	List<Pais> limitrofes;
 	
-	public Pais(){
+	public Pais(int codigo){
+		this.codigo = codigo;
 		cobertura = false;
 		limSinCobertura = 0;
 		limitrofes = new ArrayList<Pais>();
@@ -19,5 +21,17 @@ public class Pais {
 		limSinCobertura++;
 	}
 	
+	public void cubrirLimitrofes(){
+		
+	}
 	
+	@Override
+	public boolean equals(Object otro) {
+		if (this == otro)
+			return true;
+		if ( !(otro instanceof Pais) )
+			return false;
+		Pais oPais = (Pais) otro;
+		return (this.codigo == oPais.codigo);
+	}
 }
